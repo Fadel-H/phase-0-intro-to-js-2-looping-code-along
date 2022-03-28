29bbbf4be62d1e9fbf68a51792fea127be5d694c
Fadel <fadel.g.heji@gmail.com>
2022-03-27 20:33:12 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+
+let name 
+
+function writeCards(name, event) { 
+
+  const array = []
+
+  for (let i = 0; i < name.length;i++) {
+    array.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+  }
+  return array
+}
+
+function countDown(i){
+  while (i >= 0){
+    console.log(i)
+    i--
+  }
+}
+
